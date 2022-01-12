f4a779928f9fff6ce05a094c6ab46020d2dc1b2f
Robin Kennedy-Reid <robin.kennedy-reid@stfc.ac.uk>
2022-01-12 13:26:55 +0000

    Add working coffee cup examples
    Using main.py file to run different scripts

---
+++ coffee_cup/uq_coffee_class.py
@@ -0,0 +1,47 @@
+import chaospy as cp  # To create distributions
+import numpy as np  # For the time array
+import uncertainpy as un
+from scipy.integrate import odeint  # To integrate our equation
+
+
+# Create the coffee cup model
+class CoffeeCup(un.Model):
+    # Add labels to the model by calling the constructor of the parent un.Model
+    def __init__(self):
+        super(CoffeeCup, self).__init__(labels=["Time (s)", "Temperature (C)"])
+
+    # Define the run method
+    def run(self, kappa, T_env):
+        # Initial temperature and time array
+        time = np.linspace(0, 200, 150)  # Minutes
+        T_0 = 95  # Celsius
+
+        # The equation describing the model
+        def f(T, time, kappa, T_env):
+            return -kappa * (T - T_env)
+
+        # Solving the equation by integration
+        temperature = odeint(f, T_0, time, args=(kappa, T_env))[:, 0]
+
+        # Return time and model output
+        return time, temperature
+
+
+def main():
+    # Initialize the model
+    model = CoffeeCup()
+
+    # Create the distributions
+    kappa_dist = cp.Uniform(0.025, 0.075)
+    T_env_dist = cp.Uniform(15, 25)
+
+    # Define the parameters dictionary
+    parameters = {"kappa": kappa_dist, "T_env": T_env_dist}
+
+    # Set up the uncertainty quantification
+    UQ = un.UncertaintyQuantification(model=model, parameters=parameters)
+
+    # Perform the uncertainty quantification using
+    # polynomial chaos with point collocation (by default)
+    # We set the seed to easier be able to reproduce the result
+    data = UQ.quantify(seed=10)
---
+++ coffee_cup/uq_coffee_function.py
@@ -0,0 +1,41 @@
+import chaospy as cp  # To create distributions
+import numpy as np  # For the time array
+import uncertainpy as un
+from scipy.integrate import odeint  # To integrate our equation
+
+
+# Create the coffee cup model function
+def coffee_cup(kappa, T_env):
+    # Initial temperature and time array
+    time = np.linspace(0, 200, 150)  # Minutes
+    T_0 = 95  # Celsius
+
+    # The equation describing the model
+    def f(T, time, kappa, T_env):
+        return -kappa * (T - T_env)
+
+    # Solving the equation by integration
+    temperature = odeint(f, T_0, time, args=(kappa, T_env))[:, 0]
+
+    # Return time and model output
+    return time, temperature
+
+
+def main():
+    # Create a model from the coffee_cup function and add labels
+    model = un.Model(run=coffee_cup, labels=["Time (min)", "Temperature (C)"])
+
+    # Create the distributions
+    kappa_dist = cp.Uniform(0.025, 0.075)
+    T_env_dist = cp.Uniform(15, 25)
+
+    # Define the parameter dictionary
+    parameters = {"kappa": kappa_dist, "T_env": T_env_dist}
+
+    # Set up the uncertainty quantification
+    UQ = un.UncertaintyQuantification(model=model, parameters=parameters)
+
+    # Perform the uncertainty quantification using
+    # polynomial chaos with point collocation (by default)
+    # We set the seed to easier be able to reproduce the result
+    data = UQ.quantify(seed=10)
